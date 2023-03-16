ad82418e74c1961a9531bcc0ec89f98b23b3d2e2
XuanHoaDev <voxuanhoa11111998@gmail.com>
2023-03-16 23:07:30 +0700

    add netlify url

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:3000", "https://coruscating-pixie-b5384e.netlify.app"], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
