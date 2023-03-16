01581eca5f302931f02929473672ddba8b663111
XuanHoaDev <voxuanhoa11111998@gmail.com>
2023-03-17 00:06:27 +0700

    edit url in index.js

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: "https://heartfelt-blini-ea0965.netlify.app", credentials: true }));
+app.use(cors({ origin: ["https://heartfelt-blini-ea0965.netlify.app", "http://localhost:3000"], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
