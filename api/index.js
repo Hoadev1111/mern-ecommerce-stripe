8ebede2036821e5b70370950501170b8a46b4e38
XuanHoaDev <voxuanhoa11111998@gmail.com>
2023-03-17 00:15:55 +0700

    change url in index.js

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: ["https://heartfelt-blini-ea0965.netlify.app", "http://localhost:3000"], credentials: true }));
+app.use(cors({ origin: ["https://magnificent-peony-339822.netlify.app/", "http://localhost:3000"], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
