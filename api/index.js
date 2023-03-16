aaf6e6350096928f2cd9fdf56eeca3d5b5443295
XuanHoaDev <voxuanhoa11111998@gmail.com>
2023-03-16 16:01:50 +0700

    add domain to index.js

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: ["http://localhost:3000", "https://mellow-mandazi-2b8ed7.netlify.app"], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
