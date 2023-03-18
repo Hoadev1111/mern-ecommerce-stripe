b229e2fa8e18ab8ee01b91f6e6d6ed5262f5d46d
XuanHoaDev <voxuanhoa11111998@gmail.com>
2023-03-18 09:39:01 +0700

    edit index.js

--- api/index.js
+++ api/index.js
@@ -15,14 +15,19 @@ app.use(cors({
         "https://spectacular-bublanina-eee7e3.netlify.app/checkout",
         "https://spectacular-bublanina-eee7e3.netlify.app/success",
         "https://spectacular-bublanina-eee7e3.netlify.app/cancel"
-    ], credentials: true
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+    credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Hello world');
 })
+
 
 mongoose.set('strictQuery', true);
 
